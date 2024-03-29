8e4922fef62cf81b7bfeb0580fd4f7b1f1e7b61a
hertin <hertinnur751@gmail.com>
2024-03-29 12:52:07 +0700

    Pemrograman Web

---
+++ Praktikum9/sessionCall.php
@@ -0,0 +1,14 @@
+<!--Praktikum Bagian 8. Membuat Session-->
+
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
+            echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+        ?>
+</body>
+</html>
---
+++ Praktikum9/sessionCreate.php
@@ -0,0 +1,16 @@
+<!--Praktikum Bagian 8. Membuat Session-->
+
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $_SESSION["favcolor"] = "green";
+            $_SESSION["favanimal"] = "cat";
+            echo "Session variables are set.";
+        ?>
+    </body>
+</html>
